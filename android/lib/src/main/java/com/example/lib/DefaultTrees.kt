904bdd9dee934e2aa901a45039174cc6a3d9a20f
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2019-12-17 15:57:20 -0800

    Split out content into 3 different files

---
+++ android/lib/src/main/java/com/example/lib/DefaultTrees.kt
@@ -0,0 +1,128 @@
+package com.example.lib
+
+import com.crashlytics.android.Crashlytics
+import timber.log.Timber
+import java.io.File
+import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.*
+
+//---------------------------------------------
+// DebugTrees, currently no priority filtering
+//---------------------------------------------
+/**
+ * Uses Crashlytics static methods to log and logException
+ */
+class CrashlyticsTree : PriorityEnabledTree() {
+
+    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+        if (!enabled) return
+
+        // Proxy log to crashlytics
+        Crashlytics.log(priority, tag, message)
+
+        // If non-fatal, log the original throwable if one was given.
+        if (throwable is Steamclog.NonFatalException) {
+            Crashlytics.logException(throwable.wrappedThrowable ?: Throwable(message))
+        }
+    }
+}
+
+//---------------------------------------------
+// DebugTrees, currently no priority filtering
+//---------------------------------------------
+/**
+ * Reformats console output to include file and line number to log.
+ */
+class CustomDebugTree: PriorityEnabledDebugTree() {
+
+    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+
+        val prettyMessage = "${getLevelEmoji(priority) ?: ""} $message"
+
+        val logThrowable =
+            if (throwable is Steamclog.NonFatalException) {
+                // If non-fatal, log the original throwable if one was given.
+                throwable.wrappedThrowable ?: Throwable(message)
+            } else {
+                throwable
+            }
+
+        super.log(priority, createCustomStackElementTag(), prettyMessage, logThrowable)
+    }
+}
+
+/**
+ *
+ */
+class ExternalLogFileTree : PriorityEnabledDebugTree() {
+    var outputFilePath: File? = null
+    var fileNamePrefix: String = "SteamLogger"
+
+    //---------------------------------------------
+    // Reformats console output to include file and line number to log.
+    //---------------------------------------------
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return "(${element.fileName}:${element.lineNumber}):${element.methodName}"
+    }
+
+    //---------------------------------------------
+    // Allows us to print out to an external file if desired.
+    //---------------------------------------------
+    override fun log(priority: Int, tag: String?, message: String, throwable: Throwable?) {
+        printLogToExternalFile(tag, message)
+    }
+
+    //---------------------------------------------
+    // Support to write logs out to External HTML file.
+    //---------------------------------------------
+    private fun printLogToExternalFile(tag: String?, message: String) {
+        try {
+            val date = Date()
+            val fileNameTimeStamp = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(date)
+            val logTimeStamp = SimpleDateFormat("E MMM dd yyyy 'at' hh:mm:ss:SSS aaa", Locale.getDefault()).format(date)
+
+            // Create file
+            val file = getExternalFile("$fileNamePrefix-$fileNameTimeStamp.html")
+
+            // If file created or exists save logs
+            if (file != null) {
+                val writer = FileWriter(file, true)
+                writer.apply {
+                    append("<p style=\"background:lightgray;\"><strong style=\"background:lightblue;\">&nbsp&nbsp")
+                    append(logTimeStamp)
+                    append(" :&nbsp&nbsp</strong><strong>&nbsp&nbsp")
+                    append(tag)
+                    append("</strong> - ")
+                    append(message)
+                    append("</p>")
+                    flush()
+                    close()
+                }
+            }
+        } catch (e: Exception) {
+            Timber.e(tag, "HTMLFileTree failed to write into file: $e")
+        }
+    }
+
+    private fun getExternalFile(filename: String): File? {
+        return try {
+            File(outputFilePath, filename)
+        } catch (e: Exception) {
+            Timber.e("HTMLFileTree failed to getExternalFile: $e")
+            null
+        }
+    }
+
+    private fun getLogFiles(): List<String> {
+        // todo may need to ask for some permissions?
+        val filteredFiles = outputFilePath?.list { _, name -> name.contains(fileNamePrefix) }
+        return filteredFiles?.sorted() ?: emptyList()
+    }
+
+    fun deleteLogFiles() {
+        for (file in getLogFiles()) {
+            getExternalFile(file)?.delete()
+        }
+    }
+}
---
+++ android/lib/src/main/java/com/example/lib/PriorityEnabledDebugTree.kt
@@ -0,0 +1,69 @@
+package com.example.lib
+
+import android.util.Log
+import timber.log.Timber
+
+/**
+ *
+ */
+
+
+//---------------------------------------------
+// PriorityEnabledTrees, allows us to filter logs based on a specific priority level.
+//---------------------------------------------
+/**
+ *  DebugTree -> logging on Release Builds
+ */
+abstract class PriorityEnabledTree : Timber.Tree() {
+    var priorityLevel: Int = Log.VERBOSE
+    var enabled: Boolean = false
+    override fun isLoggable(priority: Int): Boolean { return priority >= priorityLevel }
+}
+
+/**
+ * DebugTree -> logging on Debug Builds
+ */
+abstract class PriorityEnabledDebugTree : Timber.DebugTree() {
+    var priorityLevel: Int = Log.VERBOSE
+    var enabled: Boolean = false
+    override fun isLoggable(priority: Int): Boolean { return priority >= priorityLevel }
+}
+
+/**
+ * Timber is using a specific call stack index to correctly generate the stack element to be used
+ * in the createStackElementTag method, which is included in a final method we have no control over.
+ * Because we are wrapping Timber calls in Steamclog,alll of our
+ * that stack call index point to our library, instead of the calling method.
+ *
+ * getStackTraceElement uses a call stack index relative to our library, BUT because we cannot override
+ * Timber.getTag, we cannot
+ */
+fun PriorityEnabledDebugTree.getStackTraceElement(): StackTraceElement {
+
+    val SC_CALL_STACK_INDEX = 8 // Need to go back 8 in the call stack to get to the actual calling method.
+
+    // ---- Taken directly from Timber ----
+    // DO NOT switch this to Thread.getCurrentThread().getStackTrace(). The test will pass
+    // because Robolectric runs them on the JVM but on Android the elements are different.
+    val stackTrace = Throwable().stackTrace
+    check(stackTrace.size > SC_CALL_STACK_INDEX) { "Synthetic stacktrace didn't have enough elements: are you using proguard?" }
+    // ------------------------------------
+
+    return stackTrace[SC_CALL_STACK_INDEX]
+}
+
+/**
+ * Since Timber's createStackElementTag is made unusable to us since getTag is final, I have created
+ * createCustomStackElementTag that makes use of our custom call stack index to give us better filename
+ * and linenumber reporting.
+ */
+fun PriorityEnabledDebugTree.createCustomStackElementTag(): String {
+    val element = getStackTraceElement()
+    return "(${element.fileName}:${element.lineNumber}):${element.methodName}"
+}
+
+fun PriorityEnabledDebugTree.getLevelEmoji(utilLevel: Int): String? = when(utilLevel) {
+    Log.ERROR -> "🚫"
+    Log.WARN -> "⚠️"
+    else -> null
+}
