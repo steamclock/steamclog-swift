3a2554887637294775204837d6c9255a454e05c5
Brendan Lensink <blensink192@gmail.com>
2020-01-21 08:43:29 -0800

    fix project name

--- ios/Example/Pods/Target Support Files/Pods-steamclog_Example/Pods-steamclog_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_steamclog_Example : NSObject
-@end
-@implementation PodsDummy_Pods_steamclog_Example
-@end
--- ios/Example/Pods/Target Support Files/steamclog/steamclog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_steamclog : NSObject
-@end
-@implementation PodsDummy_steamclog
-@end
