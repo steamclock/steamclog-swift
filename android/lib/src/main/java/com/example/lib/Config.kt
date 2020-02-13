8f833c1aee7d0d21686bde63244ea0f591d776db
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2020-02-12 18:40:11 -0800

    Updated config

--- android/lib/src/main/java/com/example/lib/Config.kt
+++ android/lib/src/main/java/com/example/lib/Config.kt
@@ -8,27 +8,37 @@ import java.io.File
  * Created by shayla on 2020-01-23
  */
 data class Config(
-    /**
-     *
-     */
-    var destinationLevels: DestinationLevels = DestinationLevels.Debug,
 
     /**
-     *
+     * todo: Should this be bundle name or something? Do we have access to that from inside the package?
      */
     var identifier: String = "steamclog",
 
     /**
-     *
+     * Destination logging levels
+     */
+    var destinationLevels: DestinationLevels = DestinationLevels.Debug,
+
+    /**
+     * todo: Not sure if we want to do Crashlytics setup here?
      */
     var crashlyticsAppKey: String? = null,
 
     /**
-     *
+     * Location where the app wishes to store any log files generated
+     * ex. externalCacheDir
+     * todo: Can we set this for the app, or should we allow this as a config.
      */
     var fileWritePath: File? = null,
 
     /**
-     *
+     *  Determines how long generated log files are kept for.
      */
-    var keepLogsForDays: Int = 3)
+    var keepLogsForDays: Int = 3,
+
+    /**
+     * Indicates if objects being logged must implement the redacted interface.
+     */
+    var requireRedacted: Boolean = false
+
+)
