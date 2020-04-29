627535d32e593669d3e9705954ad1b19d751f87c
Jacob Miner <jake@steamclock.com>
2020-04-29 13:53:29 -0700

    Make AutoRotateConfig non-mutable

--- ios/SteamcLog/SteamcLog/Classes/Helper/AutoRotateConfig.swift
+++ ios/SteamcLog/SteamcLog/Classes/Helper/AutoRotateConfig.swift
@@ -10,7 +10,7 @@ import XCGLogger
 
 public struct AutoRotateConfig {
     /// The number of seconds before the log file will be rotated out.
-    var rotateAfterSeconds: TimeInterval
+    let rotateAfterSeconds: TimeInterval
 
     public init(rotateAfterSeconds: TimeInterval = AutoRotatingFileDestination.autoRotatingFileDefaultMaxTimeInterval) {
         self.rotateAfterSeconds = rotateAfterSeconds
