168d0b425c1ffacd571933a8009f9b9b64189489
Jacob Miner <jake@steamclock.com>
2020-04-30 09:56:15 -0700

    Add links for more documentation

--- ios/SteamcLog/SteamcLog/Classes/Helper/AutoRotateConfig.swift
+++ ios/SteamcLog/SteamcLog/Classes/Helper/AutoRotateConfig.swift
@@ -12,6 +12,7 @@ public struct AutoRotateConfig {
     /// The number of seconds before the log file will be rotated out.
     let rotateAfterSeconds: TimeInterval
 
+    /// More information available in https://github.com/DaveWoodCom/XCGLogger#automatic-log-file-rotation and https://github.com/DaveWoodCom/XCGLogger/blob/master/Sources/XCGLogger/Destinations/AutoRotatingFileDestination.swift
     public init(rotateAfterSeconds: TimeInterval = AutoRotatingFileDestination.autoRotatingFileDefaultMaxTimeInterval) {
         self.rotateAfterSeconds = rotateAfterSeconds
     }
