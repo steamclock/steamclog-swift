fd70e1ad6bf7b344b2161659a429de1592328cc5
Jacob Miner <jake@steamclock.com>
2020-05-01 15:46:50 -0700

    [41] Allow requireRedacted to be settable

--- ios/SteamcLog/SteamcLog/Classes/Helper/Config.swift
+++ ios/SteamcLog/SteamcLog/Classes/Helper/Config.swift
@@ -9,15 +9,16 @@ import Foundation
 
 public struct Config {
     /// Global log threshold, logs under this level will be ignored
-    var logLevel: LogLevelPreset = .develop
+    internal let logLevel: LogLevelPreset
 
-    var identifier = "steamclog" // TODO: Should this be bundle name or something? Do we have access to that from inside the package?
+    /// Identifier used for XCGLogger destinations
+    internal let identifier: String // TODO: Should this be bundle name or something? Do we have access to that from inside the package?
 
     /// Allows customization of auto rotating of log files. By default, file will rotate every 600 seconds.
-    let autoRotateConfig: AutoRotateConfig
+    internal let autoRotateConfig: AutoRotateConfig
 
-    // Require that all logged objects conform to Redacted or are all redacted by default.
-    @usableFromInline internal var requireRedacted = false
+    /// Require that all logged objects conform to Redacted or are all redacted by default.
+    @usableFromInline internal let requireRedacted: Bool
 
     public init(logLevel: LogLevelPreset = .develop,
                 requireRedacted: Bool = false,
