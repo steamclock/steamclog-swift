40eaeefac1f4d778564db411df2dced2e575c847
Nigel Brooke <nigel@steamclock.com>
2020-07-20 17:07:33 -0700

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SteamcLog",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "SteamcLog",
+            targets: ["SteamcLog"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger", from: "7.0.0"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", from: "5.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "SteamcLog",
+            dependencies: ["XCGLogger", "Sentry"],
+            path: "SteamcLog"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
