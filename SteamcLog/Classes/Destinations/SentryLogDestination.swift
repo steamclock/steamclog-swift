dfc58b9b30f6599d38df410a4533bfe65ed9a9d0
Nigel Brooke <nigel@steamclock.com>
2020-08-05 16:59:13 -0700

    Add some more details to crashes and errors (#69)
    * Add support for capturing stacktraces for errors as well
    * Stop filing duplicate issues for fatal errors (both a captured error and the actual crash)
    * Include fatal error text as a breadcrumb on the crash as well

--- SteamcLog/Classes/Destinations/SentryLogDestination.swift
+++ SteamcLog/Classes/Destinations/SentryLogDestination.swift
@@ -30,15 +30,21 @@ class SentryDestination: BaseQueuedDestination {
     }
 
     override open func output(logDetails: LogDetails, message: String) {
-        if logDetails.level >= .error {
-                 let event = Event(level: logDetails.level.sentryLevel)
-                 event.message = logDetails.message
-                 SentrySDK.capture(event: event)
+        if logDetails.level == .severe {
+            // Note: text from fatalError does not currently seem to be captured (might be a bug on Sentry's side)
+            // so for fatal errors also log a breadcrumb with the error text, so we at least have it somewhere, putting them in a
+            // different category, just so they stand out more
+            let breadcrumb = Breadcrumb(level: .fatal, category: "fatal")
+            breadcrumb.message = logDetails.message
+            SentrySDK.addBreadcrumb(crumb: breadcrumb)
+        } else if logDetails.level == .error {
+            let event = Event(level: logDetails.level.sentryLevel)
+            event.message = logDetails.message
+            SentrySDK.capture(event: event)
         } else {
-                 let breadcrumb = Breadcrumb(level: logDetails.level.sentryLevel, category: "steamclog")
-                 breadcrumb.message = logDetails.message
-                 SentrySDK.addBreadcrumb(crumb: breadcrumb)
+            let breadcrumb = Breadcrumb(level: logDetails.level.sentryLevel, category: "steamclog")
+            breadcrumb.message = logDetails.message
+            SentrySDK.addBreadcrumb(crumb: breadcrumb)
         }
-
     }
 }
