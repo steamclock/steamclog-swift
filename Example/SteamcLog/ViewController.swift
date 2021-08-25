363f682552bbfc8cf2fc67f00b3afd32d956afc1
Jeremy Chiang <jeremy@steamclock.com>
2021-08-25 11:13:43 -0700

    Comment out and make tests for fatal errors and analytics optional, #85

--- Example/SteamcLog/ViewController.swift
+++ Example/SteamcLog/ViewController.swift
@@ -25,8 +25,13 @@ class ViewController: UIViewController {
         clog.info("No admittance except on party business.")
         clog.warn("All we have to decide is what to do with the time that is given to us.")
         clog.error("I don’t know half of you half as well as I should like and I like less than half of you half as well as you deserve.")
-        clog.fatal("It's the job that's never started as takes longest to finish.")
 
+        // Uncomment to test fatal errors.
+        // clog.fatal("It's the job that's never started as takes longest to finish.")
+
+        /*
+         * Uncomment to test analytics.
+         *
         let sampleUser = User(name: "Name", uuid: UUID(), email: "hi@steamclock.com", created: Date())
         /// Print a simple model out with some properties redacted
         clog.info("Here's a simple model: ", sampleUser)
@@ -39,6 +44,8 @@ class ViewController: UIViewController {
 
         /// Log an event with an encodable object
         clog.track(id: Event.userCreated, encodable: sampleUser)
+         *
+         */
     }
 
     @IBAction func showFileContents(_ sender: Any) {
