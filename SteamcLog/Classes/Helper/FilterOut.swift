c9a6a40f70c101361b55eb36e29a0a26ad66dabc
Jacob Miner <jake@steamclock.com>
2021-12-22 15:50:38 -0800

    Update comment

--- SteamcLog/Classes/Helper/FilterOut.swift
+++ SteamcLog/Classes/Helper/FilterOut.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /**
- * FilterOut functional interface (ie. should only contain a single method).
+ * FilterOut lambda.
  * Allows the application to intercept Errors when an error occurs and decide if the
  * Error should be blocked from being logged as an error by the crash reporting
  * destination.
